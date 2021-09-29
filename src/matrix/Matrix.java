fe34b72c00282a0413692a19030f75aff6045c25
Jordan Greene <greenejd2@appstate.edu>
2021-09-29 16:53:27 -0400

    all constructors created

--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -7,22 +7,68 @@ public class Matrix
 
     private int m;
     private int n;
-    private double [][] data;
+    private double [][] matrix;
 
+    // Issue: Create Matrix constructor from two dimension arguments initialized with constant value
     public Matrix(int m, int n)
     {
         this.m = m;
         this.n = n;
         // Java inits to zeros 
-        this.data = new double[m][n];
+        this.matrix = new double[m][n];
+    }
+
+    public Matrix(int m, int n, double s)
+    {
+        this.m = m;
+        this.n = n;
+
+        // Build matrix
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                this.matrix[i][j] = s;
+            }
+        }
+
+    }
+
+    // Issue: Create constructor from 2D array
+    public Matrix(double[][] A)
+    {
+        this.m = A.length;
+        this.n = A[0].length;
+        this.matrix = new double[m][n];
+
+        // Build matrix
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                this.matrix[i][j] = A[i][j];
+            }
+        }
+
     }
 
     public Matrix(double[][] A, int m, int n)
     {
         this.m = m;
         this.n = n;
+
         // Does this create copy or reference?
-        this.data = A;
+        // this.data = A;
+
+        // This creates copy.
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                this.matrix[i][j] = A[i][j];
+            }
+        }
+
     }
 
     /*
@@ -40,8 +86,7 @@ public class Matrix
             for (int i = 0; i < n; i++)
             {
                 // Create matrix from columns
-                // TODO!!! stopped here
-                this.data[i][j] = vals[];
+                this.matrix[i][j] = vals[i + (j + (j * 2))];
             }
         }
     }
