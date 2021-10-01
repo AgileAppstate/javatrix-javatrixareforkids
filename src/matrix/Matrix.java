72824698fa66f3144d99e9ac0d25818d4e0e2329
Matthias Sterckx <sterckx.matthias@live.com>
2021-10-01 13:48:51 -0400

    Implemented times functions for scalar and matrix multiplication

--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -5,8 +5,8 @@ public class Matrix
     implements java.lang.Cloneable, java.io.Serializable
 {
 
-    private int m;
-    private int n;
+    private int rows;
+    private int columns;
     private double [][] matrix;
 
     // Getters for testing purposes
@@ -17,27 +17,27 @@ public class Matrix
 
     public int getRows()
     {
-        return this.m;
+        return this.rows;
     }
 
     public int getColumns()
     {
-        return this.n;
+        return this.column;
     }
 
     // Issue: Create Matrix constructor from two dimension arguments initialized with constant value
     public Matrix(int m, int n)
     {
-        this.m = m;
-        this.n = n;
+        this.rows = m;
+        this.columns = n;
         // Java inits to zeros 
         this.matrix = new double[m][n];
     }
 
     public Matrix(int m, int n, double s)
     {
-        this.m = m;
-        this.n = n;
+        this.rows = m;
+        this.columns = n;
         this.matrix = new double[m][n];
 
         // Build matrix
@@ -54,8 +54,8 @@ public class Matrix
     // Issue: Create constructor from 2D array
     public Matrix(double[][] A)
     {
-        this.m = A.length;
-        this.n = A[0].length;
+        this.rows = A.length;
+        this.columns = A[0].length;
         this.matrix = new double[m][n];
 
         // Build matrix
@@ -71,8 +71,8 @@ public class Matrix
 
     public Matrix(double[][] A, int m, int n)
     {
-        this.m = m;
-        this.n = n;
+        this.rows = m;
+        this.columns = n;
         this.matrix = new double[m][n];
 
         // This creates copy.
@@ -92,8 +92,8 @@ public class Matrix
      */
     public Matrix(double[] vals, int m)
     {
-        this.m = m;
-        this.n = vals.length / m;
+        this.rows = m;
+        this.columns = vals.length / m;
         this.matrix = new double[m][n];
 
         // Create by column
@@ -107,5 +107,41 @@ public class Matrix
         }
     }
 
+    /*
+     * s - Scalar value to multiply each element in matrix with
+     */
+    public Matrix times(double s)
+    {
+        double [][] newValues = new double [this.rows][this.columns];
+        for (int i = 0; i < this.rows; i++)
+        {
+            for (int j = 0; j < this.columns; j++)
+            {
+                newValues[i][j] = this.matrix[i][j] * s;
+            }
+        }
+        return new Matrix(newValues, this.rows, this.columns);
+    }
 
+    /*
+     * B - Matrix to linearly multiply with
+     */
+    public Matrix times(Matrix B)
+    {
+        if (this.getColumns() != B.getRows())
+            throw new IllegalArgumentException();
+        double [][] newValues = new double [this.rows][this.columns];
+        for (int i = 0; i < this.rows; i++)
+        {
+            for (int j = 0; j < this.columns; j++)
+            {
+                newValues[i][j] = 0;
+                for (int k = 0; k < this.columns; k++)
+                {
+                    newValues[i][j] += (this.matrix[i][k] * B.getMatrixValues()[k][j]);
+                }
+            }
+        }
+        return new Matrix(newValues, this.rows, this.columns);
+    }
 }
